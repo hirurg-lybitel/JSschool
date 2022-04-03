29c864de7041da0ef0173ac84abc280b431fa921
hirurg-lybitel <yurashoihet@gmail.com>
2022-04-04 02:06:32 +0300

    Add task 9

---
+++ Day 1/task 9.js
@@ -0,0 +1,8 @@
+/**Напишите функцию signDetermine, которая для следующих вызовов оторазит указанные результаты.
+ * a и b - любые положительыне числа
+ * 
+ * signDetermine(-a, -b)   Both are negative
+ * signDetermine(-a, b)   a is negative, b is positive
+ * signDetermine(a, b)     Both are positive
+ * signDetermine(a, -b)    a is positive, b is negative
+*/
