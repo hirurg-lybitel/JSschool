24cfec617449c5d4e880e58a200141ac1fea835d
FalkKi <nochkorzi@gmail.com>
2022-04-27 15:49:18 +0300

    Co-authored-by: hirurg-lybitel <hirurg-lybitel@users.noreply.github.com>

--- Day 4/task 7.js
+++ Day 4/task 7.js
@@ -4,3 +4,25 @@
   * @param {number} month - номер месяца
   * @returns {string}
 */
+
+
+function getSeason(number) {
+
+
+
+  if (number > 0 && number < 2 || number === 12) {
+    return 'Winter';
+  };
+  if (number >= 3 && number <= 5) {
+    return 'Spring';
+  };
+  if (number >= 6 && number <= 8) {
+    return 'Summer';
+  }
+  else {
+    return 'Fall';
+  };
+
+};
+
+console.log(getSeason(1));
