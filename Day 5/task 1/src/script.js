04a67c94c56c5c8065e49104993eb8e875b5512f
FalkKi <nochkorzi@gmail.com>
2022-05-10 19:10:37 +0300

    События в js Co-authored-by: hirurg-lybitel <hirurg-lybitel@users.noreply.github.com>

--- Day 5/task 1/src/script.js
+++ Day 5/task 1/src/script.js
@@ -1,33 +1,41 @@
-const message = 'Калькулятор'
+const message = 'Калькулятор';
 
-document.querySelector('#header').innerHTML = message
+document.querySelector('#header').innerHTML = message;
 
-let num1 = 0
+let num1 = 0;
 let num2 = 0;
 
 
 const handleNum1Change = (event) => {
     const value = event.target.value;
-    
+
     if (Number.parseFloat(value)) {
         num1 = +value;
     } else {
-        num1 = 0
+        num1 = 0;
     };
 };
 
 
 document.getElementById("inputNum2").oninput = (event) => {
     const value = event.target.value;
-    
+
     if (Number.parseFloat(value)) {
         num2 = +value;
     } else {
-        num2 = 0
+        num2 = 0;
     };
 };
 
 
 const calc = () => {
     document.querySelector('#result').value = num1 + num2;
+};
+
+document.getElementById("clear").onclick = () => {
+    num1 = 0;
+    num2 = 0;
+    document.getElementById("inputNum1").value = '';
+    document.getElementById("inputNum2").value = '';
+    document.getElementById("result").value = '';
 };
